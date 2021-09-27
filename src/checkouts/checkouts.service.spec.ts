5502c58ebe64cd33fe42bdb17fba7373f368da4d
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2021-09-27 01:05:20 -0500

    Added Unit tests for controllers

--- src/checkouts/checkouts.service.spec.ts
+++ src/checkouts/checkouts.service.spec.ts
@@ -1,88 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CheckoutsController } from './checkouts.controller';
 import { CheckoutsService } from './checkouts.service';
-import { AppService } from './../app.service';
-import { CheckoutDTO } from './../dto/checkout';
+import { PrismaService } from '../../src/prisma.service';
 
-describe('CheckoutsController', () => {
-  let controller: CheckoutsController;
-
-  const mockProductService = {
-    create: jest.fn((data: CheckoutDTO) => {
-      return {
-        id: Date.now(),
-        ...data,
-      };
-    }),
-    update: jest.fn((data: CheckoutDTO) => {
-      return {
-        id: Date.now(),
-        ...data,
-      };
-    }),
-    getAll: jest.fn(() => []),
-    verifyIfExists: jest.fn(() => false),
-  };
+describe('CheckoutsService', () => {
+  let service: CheckoutsService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [CheckoutsController],
-      providers: [AppService, CheckoutsService],
-    })
-      .overrideProvider(CheckoutsService)
-      .useValue(mockProductService)
-      .compile();
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, CheckoutsService],
+      imports: [],
+    }).compile();
 
-    controller = app.get<CheckoutsController>(CheckoutsController);
+    service = module.get<CheckoutsService>(CheckoutsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  it('should get checkout', async () => {
-    const responseCheckout = await controller.getAll();
-
-    expect(responseCheckout).toEqual([]);
-    expect(mockProductService.getAll).toHaveBeenCalled();
-  });
-
-  it('should create a checkout', async () => {
-    const checkoutDTO: CheckoutDTO = {
-      idProduct: 1,
-      quantity: 10,
-    };
-    const checkoutResponse = {
-      product: { connect: { id: checkoutDTO.idProduct } },
-      quantity: checkoutDTO.quantity,
-    };
-
-    const checkout = await controller.create(checkoutDTO);
-
-    expect(checkout).toEqual({
-      id: expect.any(Number),
-      ...checkoutResponse,
-    });
-
-    expect(mockProductService.create).toHaveBeenCalledWith(checkoutResponse);
-  });
-  it('should update a checkout', async () => {
-    const checkoutDTO: CheckoutDTO = {
-      idProduct: 1,
-      quantity: 10,
-    };
-    const checkoutResponse = {
-      product: { connect: { id: checkoutDTO.idProduct } },
-      quantity: checkoutDTO.quantity,
-    };
-
-    const checkout = await controller.update(checkoutDTO);
-
-    expect(checkout).toEqual({
-      id: expect.any(Number),
-      ...checkoutResponse,
-    });
-
-    expect(mockProductService.create).toHaveBeenCalledWith(checkoutResponse);
+  it('CheckoutService - should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
